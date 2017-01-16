a90ea664ba4d4e47376ad7a90ac3f9334b070532
Elizabeth Jones <elizabeth.anna.jones@gmail.com>
2017-01-16 22:31:08 +1100

    added sendgrid to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,11 +92,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-     :address              => "smtp.gmail.com",
+     :address              => "smtp.sendgrid.net",
      :port                 => 587,
-     :user_name            => "thisstyleguide",
-     :password             => "guide500",
+     :user_name            => ENV['SENDGRID_USERNAME'],
+     :password             => ENV['SENDGRID_PASSWORD'],
      :authentication       => "plain",
+     :domain               => 'heroku.com',
      :enable_starttls_auto => true
   }
 end
